73517ba60d70581c70873721abe8b4f6b3b0b992
Greg Vondra <gvondra@gmail.com>
2018-02-25 13:57:38 -0600

    Add fields to member ship request

--- WebApp/JesterCreditUnion/src/app/request-membership/request-membership.component.ts
+++ WebApp/JesterCreditUnion/src/app/request-membership/request-membership.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MembershipRequest } from '../membership-request';
 @Component({
   selector: 'app-request-membership',
   templateUrl: './request-membership.component.html',
   styleUrls: ['./request-membership.component.css']
 })
 export class RequestMembershipComponent implements OnInit {
+
+  membershipRequest : MembershipRequest = new MembershipRequest();
 
   constructor() { }
 
