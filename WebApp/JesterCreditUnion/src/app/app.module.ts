cf0917bebfc4924dac61f300bbbed2cf50d05cce
Greg Vondra <gvondra@gmail.com>
2018-02-25 10:15:53 -0600

    Create jester angular web application

---
+++ WebApp/JesterCreditUnion/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router'
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent, HeaderComponent]
+})
+export class AppModule { }
