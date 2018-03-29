c6a7c48cc00fcd191c67bcaa8b2b6ffe92d77585
Greg Vondra <gvondra@gmail.com>
2018-03-29 11:26:38 -0500

    remove unused import

--- WebApp/Members/src/app/app.module.ts
+++ WebApp/Members/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router'
 import { NgModule } from '@angular/core';
 
 
