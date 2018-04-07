9f8d5f93afa3ecf814f815cd1ae7b0aa22ecf304
Greg Vondra <gvondra@gmail.com>
2018-04-07 15:40:49 -0500

    Menu service update

--- WebApp/Operations/src/app/menu.service.ts
+++ WebApp/Operations/src/app/menu.service.ts
@@ -1,13 +1,28 @@
+import 'rxjs/add/operator/toPromise';
+import { Headers, Http, URLSearchParams } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
 import { MenuItem } from './menu-item';
-import { MENU_ITEMS } from './mock-menu-items'
 
 @Injectable()
 export class MenuService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   getMenuItems() : Promise<Array<Array<MenuItem>>> {
-    return Promise.resolve(MENU_ITEMS);
+    let items = JSON.parse(sessionStorage.getItem('jcuoMenuItems'));
+    if (items) {
+      return Promise.resolve(items as Array<Array<MenuItem>>);
+    }
+    else {
+      return this.http.get(environment.baseUrl + "Menu", {
+        headers: new Headers({"Authorization": `Bearer ${localStorage.getItem('token')}`})
+      })
+      .toPromise()
+      .then(response => {
+        sessionStorage.setItem('jcuoMenuItems', JSON.stringify(response.json()));
+        return response.json();
+      })
+    }    
   }
 }
