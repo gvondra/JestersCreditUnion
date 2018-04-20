7dd1d5c0b7223a52b46d2abe4a84f35904ec010e
Greg Vondra <gvondra@gmail.com>
2018-04-20 17:58:00 -0500

    Delete loopback API pilot

--- WebApp/OperationsAPI/common/models/auth-token.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(Authtoken) {
-    Authtoken.post = function(clientId, clientSecret, audience) {
-        return Authtoken.getToken(clientId, clientSecret, audience)        
-    }
-
-};
