a8a8c5449817b6b12ef9db087b23136e7b6f7d47
Greg Vondra <gvondra@gmail.com>
2018-04-21 09:56:26 -0500

    Add common interface project

---
+++ Interfaces/Interface.Common/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Interface Common")>
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Jesters Credit Union")>
+<Assembly: AssemblyCopyright("Copyright ©Jesters Credit Union 2018")>
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c9a1eed9-17ae-4dca-b93e-72e6a014f38d")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ Interfaces/Interface.Common/WebRequest.vb
@@ -0,0 +1,42 @@
+﻿Imports Newtonsoft.Json
+Imports System.IO
+Imports System.Net
+Public Class WebRequest
+    Public Function [Get](Of T)(ByVal url As Uri,
+                                ByVal accessToken As String) As T
+        Dim webResponse As HttpWebResponse
+        Dim webRequest As HttpWebRequest _
+            = CreateWebRequest(
+                url,
+                accessToken,
+                WebRequestMethods.Http.Get
+            )
+        Dim reader As StreamReader
+        Dim result As T
+
+        webResponse = CType(webRequest.GetResponse(), HttpWebResponse)
+        Using stream As Stream = webResponse.GetResponseStream()
+            reader = New StreamReader(stream)
+            result = JsonConvert.DeserializeObject(Of T)(reader.ReadToEnd)
+        End Using
+        Return result
+    End Function
+
+    Public Function CreateWebRequest(ByVal url As Uri,
+                                ByVal accessToken As String,
+                                ByVal method As String) As HttpWebRequest
+        Dim webRequest As HttpWebRequest = CType(Net.WebRequest.Create(url), HttpWebRequest)
+
+        webRequest.Accept = "application/json"
+        If String.Compare(method, WebRequestMethods.Http.Get, True) <> 0 Then
+            webRequest.ContentType = "application/json"
+        End If
+        webRequest.UseDefaultCredentials = False
+        If String.IsNullOrEmpty(accessToken) = False Then
+            webRequest.Headers.Add("Authorization", "Bearer " & accessToken)
+        End If
+        webRequest.Method = method
+
+        Return webRequest
+    End Function
+End Class
