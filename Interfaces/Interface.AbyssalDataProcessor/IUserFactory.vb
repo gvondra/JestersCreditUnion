81e8b06113a05b6fdd124b8ea4ded23e899dfbe5
Greg Vondra <gvondra@gmail.com>
2018-04-21 09:56:50 -0500

    add interface to abyssal data processor api

---
+++ Interfaces/Interface.AbyssalDataProcessor/IUserFactory.vb
@@ -0,0 +1,3 @@
+﻿Public Interface IUserFactory
+    Function GetBySubscriberId(ByVal settings As ISettings, ByVal accessToken As String, ByVal subscriberId As String) As User
+End Interface
---
+++ Interfaces/Interface.AbyssalDataProcessor/UserFactory.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Web
+Public Class UserFactory
+    Implements IUserFactory
+
+    Public Function GetBySubscriberId(settings As ISettings, ByVal accessToken As String, subscriberId As String) As User Implements IUserFactory.GetBySubscriberId
+        Dim uriBuilder As New UriBuilder(settings.BaseAddress)
+        Dim webRequest = New WebRequest()
+
+        uriBuilder.Path &= "user"
+        uriBuilder.Query = "subscriberId=" & HttpUtility.UrlEncode(subscriberId)
+
+        Return webRequest.Get(Of User)(uriBuilder.Uri, accessToken)
+    End Function
+End Class
