40689fe1fa8c2fad9c1275629b60757268f1a03b
Greg Vondra <gvondra@gmail.com>
2018-04-21 11:33:54 -0500

    Add post method

--- Interfaces/Interface.Common/WebRequest.vb
+++ Interfaces/Interface.Common/WebRequest.vb
@@ -22,6 +22,39 @@ Public Class WebRequest
         Return result
     End Function
 
+    Public Function Post(Of T)(ByVal url As Uri,
+                                ByVal accessToken As String,
+                                ByVal serializeBody As Action(Of Stream)) As T
+
+        Dim webResponse As HttpWebResponse
+        Dim webRequest As HttpWebRequest _
+            = CreateWebRequest(
+                url,
+                accessToken,
+                WebRequestMethods.Http.Post
+            )
+        Dim reader As StreamReader
+        Dim result As T
+
+        serializeBody.Invoke(webRequest.GetRequestStream)
+
+        webResponse = CType(webRequest.GetResponse(), HttpWebResponse)
+        Using stream As Stream = webResponse.GetResponseStream()
+            reader = New StreamReader(stream)
+            result = JsonConvert.DeserializeObject(Of T)(reader.ReadToEnd)
+        End Using
+        Return result
+    End Function
+
+    Public Sub Serialize(ByVal stream As Stream, ByVal value As Object)
+        Dim serializer As JsonSerializer
+        Using writer As New StreamWriter(stream)
+            serializer = New JsonSerializer()
+            serializer.Serialize(writer, value)
+            writer.Flush()
+        End Using
+    End Sub
+
     Public Function CreateWebRequest(ByVal url As Uri,
                                 ByVal accessToken As String,
                                 ByVal method As String) As HttpWebRequest
