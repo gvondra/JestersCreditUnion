f7110e6bf8f4fbf87f66f6946006b92ccec9e07d
Greg Vondra <gvondra@gmail.com>
2018-04-22 10:54:11 -0500

    Add user administrator menu items

--- WebApp/OperationsAPI/policies/menu.js
+++ WebApp/OperationsAPI/policies/menu.js
@@ -2,11 +2,38 @@ const p = {
     name: 'menu',
     policy: (actionParams) => {
         return (req, res, next) => {
-            res.send([
-                [
-                    {Text: "Request Role", URL: "/rolerequest"}
-                ]
-            ]);
+            var menuCount = 0;
+            console.log(req.user["https://jesterscreditunion-dvlp/role-SU"]);
+            var isSuperUser = (req.user["https://jesterscreditunion-dvlp/role-SU"] && req.user["https://jesterscreditunion-dvlp/role-SU"] === "SU");
+            var section1 = null;
+            var section2 = null;
+            
+            if (isSuperUser || (req.user["https://jesterscreditunion-dvlp/role-UA"] && req.user["https://jesterscreditunion-dvlp/role-UA"] === "UA")) {
+                if (!section2) {
+                    section2 = []
+                }
+                section2.push({Text: "Users", URL: "/Users"})
+                menuCount += 1;
+                section2.push({Text: "Groups", URL: "/Groups"})
+                menuCount += 1;
+            }
+
+            // if the user has no roles the include the role request
+            if (menuCount === 0) {
+                if (!section1) {
+                    section1 = {}
+                }
+                section1.push({Text: "Request Role", URL: "/rolerequest"})
+            }
+
+            var menu = [];
+            if (section1) {
+                menu.push(section1);
+            }
+            if (section2) {
+                menu.push(section2);
+            }
+            res.send(menu);
       }
     }
 }
