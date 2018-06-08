f0a4327e2aa387efef3745ed09c6b6850778b7c1
Greg Vondra <gvondra@gmail.com>
2018-06-08 13:48:29 -0500

    update object container builder

---
+++ Core/Core.User/ObjectContainerBuilder.vb
@@ -0,0 +1,9 @@
+﻿Imports Autofac
+Public Class ObjectContainerBuilder
+    Implements IObjectContainerBuilder
+
+    Public Sub Register(builder As ContainerBuilder) Implements IObjectContainerBuilder.Register
+        builder.RegisterType(Of UserFactory)().As(Of IUserFactory)()
+        builder.RegisterType(Of UserSaver)().As(Of IUserSaver)()
+    End Sub
+End Class
