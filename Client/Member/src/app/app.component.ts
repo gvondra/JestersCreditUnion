20204af2caf8a437ea8db8a9573049503b04f566
Greg Vondra <gvondra@gmail.com>
2023-01-27 11:06:17 -0600

    start member web site

---
+++ Client/Member/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'Member';
+
+  constructor(private router: Router) {}
+
+  NavigateHome()
+  {
+      this.router.navigate(["/"]);
+  }
+}
