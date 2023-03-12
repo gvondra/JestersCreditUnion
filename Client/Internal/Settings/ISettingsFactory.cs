683ace7b95d3c345f93ffb57007c152e931add86
Greg Vondra <gvondra@gmail.com>
2023-03-12 09:17:35 -0500

    add settings factory

---
+++ Client/Internal/Settings/ISettingsFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace JCU.Internal
+{
+    public interface ISettingsFactory
+    {
+        JestersCreditUnion.Interface.ISettings CreateApi();
+        JestersCreditUnion.Interface.ISettings CreateApi(string token);
+    }
+}
---
+++ Client/Internal/Settings/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCU.Internal
+{
+    public static class Settings
+    {
+        public static string ApiBaseAddress => ConfigurationManager.AppSettings["ApiBaseAddress"];
+        public static string GoogleAuthorizationEndpoint => ConfigurationManager.AppSettings["GoogleAuthorizationEndpoint"];
+        public static string GoogleClientId => ConfigurationManager.AppSettings["GoogleClientId"];
+        public static string GoogleClientSecret => ConfigurationManager.AppSettings["GoogleClientSecret"];
+        public static string GoogleTokenEndpoint => ConfigurationManager.AppSettings["GoogleTokenEndpoint"];
+    }
+}
