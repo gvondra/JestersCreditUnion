b1a4ff30442f0425dc8cbedc2512ac2395b415db
Greg Vondra <gvondra@gmail.com>
2023-03-13 11:47:59 -0500

    project updates

---
+++ Client/Internal/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using JCU.Internal.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JCU.Internal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {            
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private MainWindowVM MainWindowVM { get; set; }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainWindowVM = new MainWindowVM();
+            DataContext = MainWindowVM;
+        }
+
+        private void CloseCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void GoToPageCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            NavigationService navigationService = navigationFrame.NavigationService;
+            JournalEntry journalEntry = navigationService.RemoveBackEntry();
+            while (journalEntry != null)
+                journalEntry = navigationService.RemoveBackEntry();
+            //NavigationService navigationService = NavigationService.GetNavigationService(navigationFrame);
+            navigationService.Navigate(new Uri((string)e.Parameter, UriKind.Relative));
+        }
+
+        private void GoogleLoginMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            GoogleLogin.ShowLoginDialog(checkAccessToken: false, owner: this);
+        }
+
+        public void AfterTokenRefresh()
+        {
+            MainWindowVM.ShowUserRole = BoolToVisibility(AccessToken.UserHasUserAdminRoleAccess());
+            MainWindowVM.ShowLogs = BoolToVisibility(AccessToken.UserHasLogReadAccess());
+            MainWindowVM.ShowWorkTaskTypeEdit = BoolToVisibility(AccessToken.UserHasTaskTypeEditAccess());
+        }
+
+        private Visibility BoolToVisibility(bool value) => value ? Visibility.Visible : Visibility.Collapsed;
+
+    }
+}
---
+++ Client/Internal/ViewModel/MainWindowVM.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows;
+
+namespace JCU.Internal.ViewModel
+{
+    public class MainWindowVM : ViewModelBase
+    {
+        private Visibility _showUserAdmin = Visibility.Collapsed;
+        private Visibility _showUserRole = Visibility.Collapsed;
+        private Visibility _showLogs = Visibility.Collapsed;
+        private Visibility _showWorkTaskTypeEdit = Visibility.Collapsed;
+
+        public Visibility ShowWorkTaskTypeEdit
+        {
+            get => _showWorkTaskTypeEdit;
+            set
+            {
+                if (_showWorkTaskTypeEdit != value)
+                {
+                    _showWorkTaskTypeEdit = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public Visibility ShowUserAdmin
+        {
+            get => _showUserAdmin;
+            set
+            {
+                if (_showUserAdmin != value)
+                {
+                    _showUserAdmin = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public Visibility ShowUserRole
+        {
+            get => _showUserRole;
+            set
+            {
+                if (_showUserRole != value)
+                {
+                    _showUserRole = value;
+                    NotifyPropertyChanged();
+                    SetShowUserAdmin();
+                }
+            }
+        }
+
+        public Visibility ShowLogs
+        {
+            get => _showLogs;
+            set
+            {
+                if (_showLogs != value)
+                {
+                    _showLogs = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private void SetShowUserAdmin()
+            => ShowUserAdmin = ShowUserRole;
+
+    }
+}
