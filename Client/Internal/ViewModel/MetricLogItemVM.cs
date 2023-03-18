80589986e112bc5e8b6fec606b79073dd8760d7e
Greg Vondra <gvondra@gmail.com>
2023-03-18 09:19:23 -0500

    add metric log

---
+++ Client/Internal/ViewModel/MetricLogItemVM.cs
@@ -0,0 +1,23 @@
+﻿using JestersCreditUnion.Interface.Models;
+using System;
+
+namespace JCU.Internal.ViewModel
+{
+    public class MetricLogItemVM : ViewModelBase
+    {
+        private readonly Metric _innerItem;
+
+        public MetricLogItemVM(Metric innerItem)
+        {
+            _innerItem = innerItem;
+        }
+
+        public DateTime? Timestamp => _innerItem.CreateTimestamp?.ToLocalTime();
+
+        public double? Magnitude => _innerItem.Magnitude != null ? Math.Round((double)_innerItem.Magnitude, 3) : default(double?);
+
+        public string Status => _innerItem.Status;
+
+        public string RequestorName => _innerItem.RequestorName;
+    }
+}
