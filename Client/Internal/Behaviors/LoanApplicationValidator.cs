8e30de0d9fe49f5afef91dcb6be88fc934acf93f
Greg Vondra <gvondra@gmail.com>
2023-04-16 11:41:36 -0500

    add loan application validator

---
+++ Client/Internal/Behaviors/LoanApplicationValidator.cs
@@ -0,0 +1,73 @@
+﻿using JCU.Internal.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCU.Internal.Behaviors
+{
+    public class LoanApplicationValidator
+    {
+        private readonly LoanApplicationVM _loanApplicationVM;
+
+        public LoanApplicationValidator(LoanApplicationVM loanApplicationVM)
+        {
+            _loanApplicationVM = loanApplicationVM;
+            _loanApplicationVM.PropertyChanged += LoanApplicationVM_PropertyChanged;
+        }
+
+        private void LoanApplicationVM_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (_loanApplicationVM[e.PropertyName] != null) 
+                _loanApplicationVM[e.PropertyName] = null;
+            switch (e.PropertyName)
+            {
+                case nameof(LoanApplicationVM.ApplicationDate):
+                    RequiredDateField(e.PropertyName, _loanApplicationVM.ApplicationDate);
+                    DateRange(e.PropertyName, _loanApplicationVM.ApplicationDate, null, DateTime.Today);
+                    break;
+                case nameof(LoanApplicationVM.BorrowerName):
+                    RequiredTextField(e.PropertyName, _loanApplicationVM.BorrowerName);
+                    break;
+                case nameof(LoanApplicationVM.BorrowerBirthDate):
+                    RequiredDateField(e.PropertyName, _loanApplicationVM.BorrowerBirthDate);
+                    DateRange(e.PropertyName, _loanApplicationVM.BorrowerBirthDate, null, DateTime.Today);
+                    break;
+            }
+        }
+
+        private void RequiredTextField(string propertyName, string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                _loanApplicationVM[propertyName] = "Is required";
+        }
+
+        private void RequiredDateField(string propertyName, DateTime? value)
+        {
+            if (!value.HasValue)
+                _loanApplicationVM[propertyName] = "Is required";
+        }
+
+        private void DateRange(string propertyName, DateTime? value, DateTime? min, DateTime? max)
+        {
+            if (value.HasValue)
+            {
+                if (min.HasValue && max.HasValue 
+                    && (value.Value < min.Value || max.Value < value.Value))
+                {
+                    _loanApplicationVM[propertyName] = $"Is outside the range {min.Value.ToString("yyyy-MM-dd")} through {max.Value.ToString("yyyy-MM-dd")}";
+                }
+                else if (min.HasValue && value.Value < min.Value)
+                {
+                    _loanApplicationVM[propertyName] = $"Is less than the minimum of {min.Value.ToString("yyyy-MM-dd")}";
+                }
+                else if (max.HasValue && max.Value < value.Value)
+                {
+                    _loanApplicationVM[propertyName] = $"Is greater than the meximum of {max.Value.ToString("yyyy-MM-dd")}";
+                }
+
+            }
+        }
+    }
+}
--- Client/Internal/ViewModel/LoanApplicationVM.cs
+++ Client/Internal/ViewModel/LoanApplicationVM.cs
@@ -1,4 +1,5 @@
-﻿using JestersCreditUnion.Interface.Models;
+﻿using JCU.Internal.Behaviors;
+using JestersCreditUnion.Interface.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,9 +32,50 @@ namespace JCU.Internal.ViewModel
             }
         }
 
+        public DateTime? ApplicationDate 
+        { 
+            get => _loanApplication.ApplicationDate;
+            set
+            {
+                if (_loanApplication.ApplicationDate.HasValue != value.HasValue 
+                    || (value.HasValue && _loanApplication.ApplicationDate.Value != value.Value))
+                {
+                    _loanApplication.ApplicationDate = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public string BorrowerName 
+        { 
+            get => _loanApplication.BorrowerName ?? string.Empty;
+            set
+            {
+                if (_loanApplication.BorrowerName != value)
+                {
+                    _loanApplication.BorrowerName = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public DateTime? BorrowerBirthDate 
+        { 
+            get => _loanApplication.BorrowerBirthDate;
+            set
+            {
+                if (_loanApplication.BorrowerBirthDate.HasValue != value.HasValue
+                    || (value.HasValue && _loanApplication.BorrowerBirthDate.Value != value.Value))
+                {
+                    _loanApplication.BorrowerBirthDate = value;
+                    NotifyPropertyChanged();
+                }
+            } 
+        }
+
         public static LoanApplicationVM Create(LoanApplication loanApplication)
         {
             LoanApplicationVM vm = new LoanApplicationVM(loanApplication);
+            LoanApplicationValidator validator = new LoanApplicationValidator(vm);
+            vm.AddBehavior(validator);
             return vm;
         }
     }
