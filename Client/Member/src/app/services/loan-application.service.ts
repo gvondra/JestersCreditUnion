7812dd70d71da2470492d6b251a45343ecf7b81e
Greg Vondra <gvondra@gmail.com>
2023-04-29 11:53:45 -0500

    add search method

--- Client/Member/src/app/services/loan-application.service.ts
+++ Client/Member/src/app/services/loan-application.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { mergeMap, Observable } from 'rxjs';
 import { HttpClientUtilService } from '../http-client-util.service';
@@ -13,6 +13,18 @@ export class LoanApplicationService {
   constructor(private httpClientUtil: HttpClientUtilService,
     private httpClient: HttpClient,
     private tokenService: TokenService) { }
+
+  Search(byRequestor: boolean = true) : Observable<LoanApplication[]> {
+    let params: HttpParams = new HttpParams();
+    if (byRequestor) {
+      params = params.append("byRequestor", "true")
+    }
+    return this.httpClientUtil.CreateAuthHeader(this.tokenService).pipe(
+      mergeMap((httpHeaders: HttpHeaders) => {
+        return this.httpClient.get<LoanApplication[]>(`${this.httpClientUtil.GetApiBaseAddress()}LoanApplication`, {headers: httpHeaders, params: params})
+      })
+    );
+  }
 
   Create(loanApplication: LoanApplication): Observable<LoanApplication> {
     return this.httpClientUtil.CreateAuthHeader(this.tokenService).pipe(
