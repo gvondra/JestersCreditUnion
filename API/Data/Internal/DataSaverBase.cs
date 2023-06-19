bfbcfba82ff5e6db2f7e6709a8fef005b867ea71
Greg Vondra <gvondra@gmail.com>
2023-06-19 09:26:26 -0500

    add data client specific base classes

---
+++ API/Data/Internal/DataSaverBase.cs
@@ -0,0 +1,12 @@
+﻿namespace JestersCreditUnion.Data.Internal
+{
+    public abstract class DataSaverBase
+    {
+        protected readonly IDbProviderFactory _providerFactory;
+
+        public DataSaverBase(IDbProviderFactory providerFactory)
+        {
+            _providerFactory = providerFactory;
+        }
+    }
+}
