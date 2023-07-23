bc33e2cc1e111172997a0a4de19530617bba0d4b
Greg Vondra <gvondra@gmail.com>
2023-07-23 08:48:45 -0500

    add date generator

--- Testing/TestLoanGenerator/DependencyInjection/ContainerModule.cs
+++ Testing/TestLoanGenerator/DependencyInjection/ContainerModule.cs
@@ -7,6 +7,9 @@ namespace JestersCreditUnion.Testing.LoanGenerator.DependencyInjection
         protected override void Load(ContainerBuilder builder)
         {
             base.Load(builder);
+            builder.RegisterType<DateGenerator>()
+                .As<IDateGenerator>()
+                .SingleInstance();
             builder.RegisterType<NameGenerator>()
                 .As<INameGenerator>()
                 .SingleInstance();
---
+++ Testing/TestLoanGenerator/IDateGenerator.cs
@@ -0,0 +1,8 @@
+﻿namespace JestersCreditUnion.Testing.LoanGenerator
+{
+    public interface IDateGenerator
+    {
+        DateTime GenerateDate(int? dayRange, int? yearRange);
+        DateTime GenerateDate(DateTime seed, int? dayRange, int? yearRange);
+    }
+}
