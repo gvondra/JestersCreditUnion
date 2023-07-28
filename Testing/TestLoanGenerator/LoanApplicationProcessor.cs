327895b41443b932ee5b70eb1a9aa450c52846a1
Greg Vondra <gvondra@gmail.com>
2023-07-28 11:51:19 -0500

    generator updates

--- Testing/TestLoanGenerator/LoanApplicationProcessor.cs
+++ Testing/TestLoanGenerator/LoanApplicationProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using JestersCreditUnion.Interface;
 using JestersCreditUnion.Interface.Models;
+using Serilog;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -10,17 +11,20 @@ namespace JestersCreditUnion.Testing.LoanGenerator
         private readonly ISettingsFactory _settingsFactory;
         private readonly IEnumerable<LoanApplication> _loanApplications;
         private readonly ILoanApplicationService _loanApplicationService;
+        private readonly ILogger _logger;
 
         public LoanApplicationProcessor(
             ISettingsFactory settingsFactory,
             Settings settings,
             ILoanApplicationService loanApplicationService,
+            ILogger logger,
             Func<int, ILoanApplicationGenerator> createLoanApplicationGenerator,
             Func<string, IEnumerable<LoanApplication>, ILoanApplicationFileWriter> createLoanApplicationFileWriter)
         {
             _settingsFactory = settingsFactory;
             _loanApplications = createLoanApplicationGenerator(settings.LoanApplicationCount);
             _loanApplicationService = loanApplicationService;
+            _logger = logger;
             if (!string.IsNullOrEmpty(settings.LoanApplicationFile))
                 _loanApplications = createLoanApplicationFileWriter(settings.LoanApplicationFile, _loanApplications);
         }
@@ -28,10 +32,17 @@ namespace JestersCreditUnion.Testing.LoanGenerator
         public async Task GenerateLoanApplications()
         {
             ApiSettings apiSettings = await _settingsFactory.GetApiSettings();
+            Queue<Task> createQueue = new Queue<Task>();
             foreach (LoanApplication loanApplication in _loanApplications)
             {
-                await _loanApplicationService.Create(apiSettings, loanApplication);
+                while (createQueue.Count >= 3)
+                {
+                    await createQueue.Dequeue();
+                }
+                _logger.Information($"Creating loan. Borrower {loanApplication.BorrowerName}");
+                createQueue.Enqueue(_loanApplicationService.Create(apiSettings, loanApplication));
             }
+            await Task.WhenAll(createQueue);
         }
     }
 }
