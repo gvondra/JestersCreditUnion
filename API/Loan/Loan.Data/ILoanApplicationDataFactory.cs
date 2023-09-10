46c81d6ba39beb2377401b899a91ef4154e90bf5
Greg Vondra <gvondra@gmail.com>
2023-09-10 10:31:33 -0500

    move data tier

---
+++ API/Loan/Loan.Data/ILoanApplicationDataFactory.cs
@@ -0,0 +1,12 @@
+﻿using JestersCreditUnion.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace JestersCreditUnion.Data
+{
+    public interface ILoanApplicationDataFactory
+    {
+        Task<LoanApplicationData> Get(ISqlSettings settings, Guid id);
+        Task<IEnumerable<LoanApplicationData>> GetByUserId(ISqlSettings settings, Guid userId);
+    }
+}
