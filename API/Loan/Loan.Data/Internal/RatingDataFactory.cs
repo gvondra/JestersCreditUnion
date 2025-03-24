58a094f9137e5847214a159e569c528b262f4ec3
Greg Vondra <greg.vondra@outlook.com>
2025-03-24 08:33:27 -0500

    update rating data factory for mysql

--- API/Loan/Loan.Data/Internal/RatingDataFactory.cs
+++ API/Loan/Loan.Data/Internal/RatingDataFactory.cs
@@ -1,42 +1,73 @@
 ﻿using JestersCreditUnion.Loan.Data.Models;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace JestersCreditUnion.Loan.Data.Internal
 {
     public class RatingDataFactory : DataFactoryBase<RatingData>, IRatingDataFactory
     {
-        public RatingDataFactory(IDbProviderFactory providerFactory, IGenericDataFactory<RatingData> dataFactory)
+        private readonly IGenericDataFactory<RatingLogData> _ratingLogDataFactory;
+
+        public RatingDataFactory(IDbProviderFactory providerFactory, IGenericDataFactory<RatingData> dataFactory, IGenericDataFactory<RatingLogData> ratingLogDataFactory)
             : base(providerFactory, dataFactory)
-        { }
+        {
+            _ratingLogDataFactory = ratingLogDataFactory;
+        }
 
         public async Task<RatingData> GetByLoanApplicationId(ISettings settings, Guid loanApplicationId)
         {
             IDataParameter[] parameters =
             [
-                DataUtil.CreateParameter(ProviderFactory, "loanApplicationId", DbType.Guid, DataUtil.GetParameterValue(loanApplicationId))
+                DataUtil.CreateParameter(ProviderFactory, "loanApplicationId", DbType.Binary, DataUtil.GetParameterValueBinary(loanApplicationId))
             ];
             DataReaderProcess dataReaderProcess = new DataReaderProcess();
-            RatingData data = null;
-            await dataReaderProcess.Read(
+            IEnumerable<RatingData> result = await dataReaderProcess.Read(
                 settings,
                 ProviderFactory,
-                "[ln].[GetRating_by_LoanApplicationId]",
-                commandType: CommandType.StoredProcedure,
+                GetByLoanApplicationIdSql(),
+                commandType: CommandType.Text,
                 parameters: parameters,
-                async (DbDataReader reader) =>
+                Read);
+            return result?.FirstOrDefault();
+        }
+
+        private static string GetByLoanApplicationIdSql()
+        {
+            StringBuilder sql = new StringBuilder("SELECT ");
+            sql.AppendLine(string.Join(", ", Constants.Column.Rating.Select(c => $"`rt`.`{c}`")));
+            sql.AppendLine($"FROM `{Constants.TableName.Rating}` `rt` ");
+            sql.AppendLine($"INNER JOIN `{Constants.TableName.LoanApplicationRating}` `lar` ON `rt`.`RatingId` = `lar`.`RatingId` ");
+            sql.AppendLine("WHERE `lar`.`LoanApplicationId` = @loanApplicationId; ");
+
+            sql.Append("SELECT ");
+            sql.AppendLine(string.Join(", ", Constants.Column.RatingLog.Select(c => $"`lg`.`{c}`")));
+            sql.AppendLine($"FROM `{Constants.TableName.RatingLog}` `lg` ");
+            sql.AppendLine($"INNER JOIN `{Constants.TableName.LoanApplicationRating}` `lar` ON `lg`.`RatingId` = `lar`.`RatingId` ");
+            sql.AppendLine("WHERE `lar`.`LoanApplicationId` = @loanApplicationId; ");
+
+            return sql.ToString();
+        }
+
+        private async Task<IEnumerable<RatingData>> Read(DbDataReader reader)
+        {
+            IEnumerable<RatingData> result = await DataFactory.LoadData(reader, Create, DataUtil.AssignDataStateManager);
+            await reader.NextResultAsync();
+            IEnumerable<RatingLogData> logData = await _ratingLogDataFactory.LoadData(reader, () => new RatingLogData(), DataUtil.AssignDataStateManager);
+            return result
+                .GroupJoin(
+                logData,
+                r => r.RatingId,
+                lg => lg.RatingId,
+                (r, lg) =>
                 {
-                    data = (await DataFactory.LoadData(reader, Create, DataUtil.AssignDataStateManager)).FirstOrDefault();
-                    if (data != null)
-                    {
-                        GenericDataFactory<RatingLogData> ratingLogFactory = new GenericDataFactory<RatingLogData>();
-                        await reader.NextResultAsync();
-                        data.RatingLogs = (await ratingLogFactory.LoadData(reader, () => new RatingLogData(), DataUtil.AssignDataStateManager)).ToList();
-                    }
-                });
-            return data;
+                    r.RatingLogs = lg.ToList();
+                    return r;
+                })
+                .ToList();
         }
     }
 }
